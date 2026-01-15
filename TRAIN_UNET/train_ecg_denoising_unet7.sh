0a3d791ada13cb68f77f5c2ea99f25249b840157
kesju <kesju@live.com>
2026-01-15 14:58:24 +0000

    pataisymai TRAIN_UNET

--- TRAIN_UNET/train_ecg_denoising_unet7.sh
+++ TRAIN_UNET/train_ecg_denoising_unet7.sh
@@ -5,15 +5,16 @@
 
 # Čia standardiniai parametrai, kurie buvo naudojami treniruojant modelį.
 python train_ecg_denoising_unet7.py\
-  --ecg_records_dir "$HOME/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/TRAIN_UNET/ecg_selected_for_test"\
+  --ecg_records_dir "$HOME/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/DATA_FOR_TRAINING"\
   --model_output "$HOME/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/TRAIN_UNET/results/resunet_ecg_1024_0_5_0_7_test.keras"\
   --vis_dir "$HOME/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/TRAIN_UNET/visualisations"\
   --train_split 0.8\
+  --log_level WARNING\
 
 # Čia  parametrai testavimui
 # python train_ecg_denoising_unet7.py\
 #   --ecg_records_dir "$HOME/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/TRAIN_UNET/ecg_selected_for_test"\
-#   --model_output "$HOME/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/TRAIN_UNET/results/resunet_ecg_1024_0_5_3_7.keras"\
+#   --model_output "$HOME/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/TRAIN_UNET/results/resunet_ecg_1024_0_5_3_7_test.keras"\
 #   --vis_dir "$HOME/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/TRAIN_UNET/visualisations"\
 #   --train_split 0.8\
 #   --log_level WARNING\
