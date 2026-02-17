bb03c4fac82d41a91658c7363324cf56488b8e5a
kesju <kesju@live.com>
2026-02-17 17:03:44 +0200

    Add new Excel file for Nika's data preparation in UNET training

--- 0_SELECT_ZIVE_DATA_2023/update_records_list_modif_v7.sh
+++ 0_SELECT_ZIVE_DATA_2023/update_records_list_modif_v7.sh
@@ -4,4 +4,4 @@ python update_records_list_modif_v7.py \
   --excel "visi_zive_irasai_atrankai._modif_v1 - Darb.xlsx" \
   --zip "Anotuoti_ir_atrinkti_duomenys" \
    --fs 200 \
-  --out "visi_zive_irasai_atrankai._modif_v1 - Darb_updated.xlsx"
+  --out "visi_zive_irasai_atrankai._modif_v1 - Darb_updated.xlsx" 
