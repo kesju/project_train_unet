19aa220f5f64c1262221803726f103b0ed95f556
kesju <kesju@live.com>
2026-02-18 18:06:39 +0200

    Update analyze_records_2026_v5.sh to include new data processing command and add records summary Excel file

--- 0_SELECT_ZIVE_DATA_2023/analyze_records_2026_v5.sh
+++ 0_SELECT_ZIVE_DATA_2023/analyze_records_2026_v5.sh
@@ -3,15 +3,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python analyze_records_2026_v5.py \
-  "1_5_Anotacijos_logai/recordings_5.zip" \
-  --fs 200 \
-  --out "1_5_Anotacijos_logai/records_summary_recordings_5-5.xlsx"
+# python analyze_records_2026_v5.py \
+#   "1_5_Anotacijos_logai/recordings_5.zip" \
+#   --fs 200 \
+#   --out "1_5_Anotacijos_logai/records_summary_recordings_5-5.xlsx"
 
 
 # Testavimui
-# python analyze_records_2026_v4.py \
-#   "/home/kesju/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/0_COLLECT_ZIVE_DATA/DUOM_2026_01_27/Igno/4_records_igno_2026" \
-#   --fs 200 \
-#   --out "records_summary_2026_v4_IGNO_4.csv"
+python analyze_records_2026_v5.py \
+  "/home/kesju/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/0_SELECT_ZIVE_DATA_2023/AtsisiuntimasZiveDuomenu/Anotuoti_visi_duomenys" \
+  --fs 200 \
+  --out "records_summary_2026_v5_visi_atsisiusti.csv"
 
