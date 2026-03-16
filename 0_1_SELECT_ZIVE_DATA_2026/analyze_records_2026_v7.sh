da9a01b66718827554ca66b8fb5232d2e69831d2
kesju <kesju@live.com>
2026-03-16 17:47:40 +0200

    Add script to generate Excel summary from ECG data and JSON metadata
    - Implemented a new Python script `analyze_records_2026_v6_2.py` that scans a directory for ECG data files and JSON metadata.
    - The script computes and exports a summary workbook in Excel format, including various statistics and metadata.
    - Introduced data classes `RecordSummary` and `OutputRow` to structure the data.
    - Added functions for loading JSON, summarizing records, and computing noise and ectopy statistics.
    - Integrated denoising and ectopy detection pipelines for processing ECG data.
    - The output Excel file includes columns for various metrics, with placeholders for manual editing.

--- 0_1_SELECT_ZIVE_DATA_2026/analyze_records_2026_v7.sh
+++ 0_1_SELECT_ZIVE_DATA_2026/analyze_records_2026_v7.sh
@@ -1,26 +1,7 @@
-# chmod +x analyze_records_2026_v6.sh
-
-# 
-
-# python analyze_records_2026_v6.py \
-#   --dir "/home/kesju/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/0_SELECT_ZIVE_DATA_2023/AtsisiuntimasZiveDuomenu/Atsisiusti_visi_anotuoti_duomenys" \
-#   --fs 200 \
-#   --cfg-denoising "/home/kesju/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/denoising_config.yaml" \
-#   --model-dir /home/kesju/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/MODEL_UNET \
-#   # --denoising \
-#   # --out "/home/kesju/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/0_SELECT_ZIVE_DATA_2023/AtsisiuntimasZiveDuomenu/DuomenysTestui/zive_irasai_testui_added_ml_noise.xlsx"  
-#   # --quiet \
-
-
-# python analyze_records_2026_v6_1.py \
-#   --cfg-denoising "/home/kesju/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/denoising_config.yaml" \
-#   --model-dir /home/kesju/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/MODEL_UNET \
-#   --dir AtsisiustiDuomenys_2026/KJ_-_2026-03-13_2225_to_2026-03-14_0636_8h_10min_test/69b525c88534dd1619e9675f/recordings \
-#   --fs 200 \
-#   --out AtsisiustiDuomenys_2026/KJ_-_2026-03-13_2225_to_2026-03-14_0636_8h_10min_test/KJ_-_2026-03-13_2225_to_2026-03-14_0636_8h_10min_test_records_summary.xlsx
+# chmod +x analyze_records_2026_v7.sh
 
 # GUNDAS
-  python analyze_records_2026_v6_3.py \
+  python analyze_records_2026_v7.py \
   --dir AtsisiustiDuomenys_2026/KJ_-_2026-03-13_2225_to_2026-03-14_0636_8h_10min_test/69b525c88534dd1619e9675f/recordings \
   --fs 200 \
   --cfg-denoising "/home/kesju/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/denoising_config.yaml" \
@@ -31,7 +12,7 @@
   --out AtsisiustiDuomenys_2026/KJ_-_2026-03-13_2225_to_2026-03-14_0636_8h_10min_test/KJ_-_2026-03-13_2225_to_2026-03-14_0636_8h_10min_test_records_summary.xlsx
 
 # # POVILAS SERVER
-#   python analyze_records_2026_v6_3.py \
+#   python analyze_records_2026_v7.py \
 #   --dir AtsisiustiDuomenys_2026/KJ_-_2026-03-13_2225_to_2026-03-14_0636_8h_10min/69b525c88534dd1619e9675f/recordings \
 #   --fs 200 \
 #   --cfg-denoising "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/denoising_config.yaml" \
