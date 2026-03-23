29e387d01eecf72e10ac4d52b93fd886c4e25f0b
kesju <kesju@live.com>
2026-03-23 11:48:11 +0200

    feat: Update script references in GUNDAS and POVILAS shell scripts to use analyze_records_2026_v7.py

--- 0_1_SELECT_ZIVE_DATA_2026/analyze_records_2026_v7_GUNDAS.sh
+++ 0_1_SELECT_ZIVE_DATA_2026/analyze_records_2026_v7_GUNDAS.sh
@@ -1,7 +1,7 @@
 # chmod +x analyze_records_2026_v7.sh
 
 # GUNDAS
-  python analyze_records_2026_v7_1.py \
+  python analyze_records_2026_v7.py \
   --dir AtsisiustiDuomenys_2026/KJ_-_2026-03-13_2225_to_2026-03-14_0636_8h_10min_test/69b525c88534dd1619e9675f/recordings \
   --fs 200 \
   --cfg-denoising "/home/kesju/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/denoising_config.yaml" \
