f746442548572bdd342c2840a581176c8ff144d3
kesju <kesju@live.com>
2026-04-11 20:06:29 +0300

    Update paths in the evaluation script for ectopic beats detection

--- 0_SELECT_ZIVE_DATA_2023/UPDATE_RECORDS_LIST_ADDING_ML_NOISES/evaluation_of_accuracy_of_ectopic_beats_detection_POVILAS.sh
+++ 0_SELECT_ZIVE_DATA_2023/UPDATE_RECORDS_LIST_ADDING_ML_NOISES/evaluation_of_accuracy_of_ectopic_beats_detection_POVILAS.sh
@@ -12,16 +12,13 @@ LOG_FILE="$RESULTS_DIR/evaluation_POVILAS_${STAMP}.log"
 SUMMARY_FILE="$RESULTS_DIR/evaluation_POVILAS_${STAMP}_summary.txt"
 
 python -u evaluation_of_accuracy_of_ectopic_beats_detection_v4.py \
-  --dir "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/0_SELECT_ZIVE_DATA_2023/AtsisiuntimasZiveDuomenu/Atsisiusti_visi_anotuoti_duomenys" \
-  --exclude-list "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/0_SELECT_ZIVE_DATA_2023/AtsisiuntimasZiveDuomenu/Atsisiusti_visi_anotuoti_duomenys/exclude_list.txt" \
-  --cfg-denoising "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/denoising_config.yaml" \
-  --unet-model-dir "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/MODEL_UNET" \
-  --denoising \
-  --disable-motions \
-  --cfg-ectopy "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/ectopy_config.yaml" \
-  --ectopy-model-dir "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/MODEL_VU_CNN" \
+  --dir "~/DI/2025_ZIVEO/DUOMENYS_ORIG/ecg_npy_all_vu" \
+  --exclude-list "~/DI/2025_ZIVEO/DUOMENYS_ORIG/ecg_npy_all_vu/exclude_list.txt" \
+  --cfg-denoising "~/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/denoising_config.yaml" \
+  --unet-model-dir "~/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/MODEL_UNET" \
+  --cfg-ectopy "~/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/ectopy_config.yaml" \
+  --ectopy-model-dir "~/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/MODEL_VU_CNN" \
   --fs 200 \
-  --all-records \
   --global-binary-metrics \
   --quiet \
   --summary-out "$SUMMARY_FILE" \
@@ -31,5 +28,26 @@ echo "Done."
 echo "Full log    : $LOG_FILE"
 echo "Clean summary: $SUMMARY_FILE"
 
+
+# python -u evaluation_of_accuracy_of_ectopic_beats_detection_v4.py \
+#   --dir "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/0_SELECT_ZIVE_DATA_2023/AtsisiuntimasZiveDuomenu/Atsisiusti_visi_anotuoti_duomenys" \
+#   --exclude-list "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/0_SELECT_ZIVE_DATA_2023/AtsisiuntimasZiveDuomenu/Atsisiusti_visi_anotuoti_duomenys/exclude_list.txt" \
+#   --cfg-denoising "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/denoising_config.yaml" \
+#   --unet-model-dir "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/MODEL_UNET" \
+#   --cfg-ectopy "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/CONFIG/ectopy_config.yaml" \
+#   --ectopy-model-dir "/home/kestutis/DI/2025_ZIVEO/PROJECT_TRAIN_UNET/MODEL_VU_CNN" \
+#   --fs 200 \
+#   --global-binary-metrics \
+#   --quiet \
+#   --summary-out "$SUMMARY_FILE" \
+#  2>&1 | tee "$LOG_FILE"
+
+# echo "Done."
+# echo "Full log    : $LOG_FILE"
+# echo "Clean summary: $SUMMARY_FILE"
+
+  # --denoising \
+  # --disable-motions \
+  # --all-records \
 
 
